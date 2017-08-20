83893ea4375eeced902f0a085cc17a5486ce356f
Jexeones24 <jexeones@gmail.com>
2017-08-20 15:10:11 -0400

    updates to application.rb and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 pic = ["https://images.unsplash.com/photo-1464658824763-547203e5a4a1?dpr=1&auto=compress,format&fit=crop&w=767&h=511&q=80&cs=tinysrgb&crop=", "https://images.unsplash.com/photo-1441794016917-7b6933969960?dpr=2&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=", "https://images.unsplash.com/photo-1452570053594-1b985d6ea890?dpr=2&auto=format&fit=crop&w=1199&h=1799&q=80&cs=tinysrgb&crop=", "https://images.unsplash.com/photo-1502842113467-03fdcac39fe5?dpr=2&auto=format&fit=crop&w=1199&h=1799&q=80&cs=tinysrgb&crop=", "https://images.unsplash.com/photo-1490376216145-7a4afd699aef?dpr=2&auto=format&fit=crop&w=1199&h=1499&q=80&cs=tinysrgb&crop="]
 
 pic.each do |pic|
-  Picture.create(url: pic, caption: "blah")
+  Picture.create(url: pic, caption: "beach")
 end
