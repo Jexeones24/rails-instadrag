595b83918a1bc207a0ca5a8af1491bfd4ea0107b
Jexeones24 <jexeones@gmail.com>
2017-08-21 10:44:03 -0400

    fixed pic params

--- app/controllers/Api/V1/pictures_controller.rb
+++ app/controllers/Api/V1/pictures_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::PicturesController < ApplicationController
   private
 
   def pic_params
-    params.require(:picture).permit(:url, :caption)
+    params.require(:picture).permit(:url, :caption, :category)
   end
 
 end
