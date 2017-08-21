c34deb8c4e9729b48ee584d7f5162aae4d8322f4
Jexeones24 <jexeones@gmail.com>
2017-08-21 14:48:31 -0400

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821143636) do
+ActiveRecord::Schema.define(version: 20170821144650) do
 
   create_table "captions", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170821143636) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
