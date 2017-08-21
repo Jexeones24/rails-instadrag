b7e833f925f1dc38e7d368077819b5d91187df3d
Jexeones24 <jexeones@gmail.com>
2017-08-21 17:24:41 -0400

    auth works

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
 		namespace :v1 do
 			resources :pictures, :categories, :captions, :users
+      post '/sessions', to: 'sessions#create'
+      get '/current_user', to: 'sessions#show'
 		end
 	end
 end
